ae3058a5312ef3429d7918054eed2095e4b53d9c
Ayres Roux <ayresrouxio@gmail.com>
2024-12-20 16:54:17 -0300

    aula: método reduce()

--- scripts.js
+++ scripts.js
@@ -1,7 +1,18 @@
-// O método some() testa se ao menos um dos elementos no array passa na condição e retorna um valor true ou false.
+/*
+  O método reduce() é utilizado para reduzir um array a um único valor.
 
-// Exemplo de array de idades.
-const ages = [15, 30, 39, 29]
+  Parâmetros:
+  - Array original (values)
+  - Acumulador (accumulator)
+  - Valor da iteração (currentValue)
+  - Valor inicial (0)
+  - Index (index da iteração atual - opcional)
+*/
 
-const result = ages.some((age) => age < 18)
-console.log(result)
+const values = [1, 2, 3, 4, 5]
+
+const sum = values.reduce((accumulator, currentValue, index) => {
+  return accumulator + currentValue
+}, 0)
+
+console.log(sum)
