a71cbfd337eaaa7a798488f9f7cfbc441a51c5b3
Ayres Roux <ayresrouxio@gmail.com>
2024-12-23 10:02:59 -0300

    aula: importando tudo

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { sum, multiply } from "./calc.js"
+import * as calc from "./calc.js"
 
-console.log("4 + 6 =", sum(4, 6))
-console.log("4 * 6 =", multiply(4, 6))
+console.log("4 + 6 =", calc.sum(4, 6))
+console.log("4 * 6 =", calc.multiply(4, 6))
