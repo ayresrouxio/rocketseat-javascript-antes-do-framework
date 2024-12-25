d6f6e2cbb7f8f37094a4f68f497c61ca74b91f3d
Ayres Roux <ayresrouxio@gmail.com>
2024-12-25 08:46:14 -0300

    aula: utilizando o day.js pelo browser

--- main.js
+++ main.js
@@ -1,20 +1,2 @@
-// (1) Executa o código de forma síncrona e o valor 1 é impresso imediatamente no console.
-console.log(1)
-
-// Microtasks são executadas antes de temporizadores e promessas.
-queueMicrotask(() => {
-  console.log(2)
-})
-
-// (5) Macrotask que aguarda o evento de temporizador ser acionado.
-setTimeout(() => {
-  console.log(3)
-}, 1000)
-
-// (2) Execução síncrona.
-console.log(4)
-
-// (4) Adiciona uma microtask.
-Promise.resolve(true).then(() => {
-  console.log(5)
-})
+const now = dayjs()
+console.log(now.format("DD/MM/YYYY - HH:mm"))
