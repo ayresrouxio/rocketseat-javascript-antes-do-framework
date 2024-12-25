c494a4e39881aa3a8820162844d9692c490f3230
Ayres Roux <ayresrouxio@gmail.com>
2024-12-25 09:49:12 -0300

    aula: utilizando async e await

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
+/*
+Utilizando o fetch com then.
+
 fetch("http://localhost:3333/products")
   .then((response) => response.json())
   .then((data) => console.log(data))
+*/
+
+// Utilizando fetch com async/await.
+async function fetchProducts() {
+  const response = await fetch("http://localhost:3333/products")
+  const data = await response.json()
+
+  console.log(data)
+}
+
+fetchProducts()
