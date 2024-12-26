1b9e3fc53fa8b1b13db737c7bd55475b6efb3ddd
Ayres Roux <ayresrouxio@gmail.com>
2024-12-26 07:28:31 -0300

    aula: instalando e executando webpack

--- dist/main.js
+++ dist/main.js
@@ -1,17 +1,1 @@
-"use strict";
-
-class User {
-  constructor(_ref) {
-    let {
-      email
-    } = _ref;
-    this.email = email;
-  }
-  sendMessage() {
-    console.log("Mensagem enviada para:", this.email);
-  }
-}
-let user = new User({
-  email: "rodrigo@email.com"
-});
-user.sendMessage();
+(()=>{"use strict";!function(){const t=document.createElement("h1");t.textContent="Hello World!",document.body.appendChild(t)}()})();
